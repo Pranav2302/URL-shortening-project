f498e097a979d573d48b0a861447c1c0f6c80d99
Pranav Kamble <98037916+Pranav2302@users.noreply.github.com>
2024-11-19 02:01:12 +0530

    Delete routes directory

--- routes/urls.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import {handelNewUrl,handleanalytics} from "../controller/user_controller.js"
-const router = express.Router();
-
-router.post('/',handelNewUrl)
-
-router.get('/analytics/:shortid',handleanalytics)
-export default router;
